815d784bf11705571ab952a8741e5b234eda4f53
Ninni-cfm <k.nienhaus@cfm-net.de>
2021-07-23 00:44:08 +0200

    Dark Mode added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,53 @@
 /*****************************************************************************
 
 */
-function switchModetest() {
-
-    var styles = window.getComputedStyle(document.body);
-
-    var fgColor = styles.color;
-    var bgColor = styles.backgroundColor;
-
-    fgColor = invertColor(fgColor);
-    bgColor = invertColor(bgColor);
-
-    document.body.style.color = fgColor;
-    document.body.style.backgroundColor = bgColor;
-}
-
-
 function switchMode() {
 
-    let x = document.getElementsByClassName("dark-mode");
+    invertForeAndBackgroundColors("dark-mode");
 
+}
+
+
+
+
+function invertForeAndBackgroundColors(className) {
+
+    // get all elements with the given class name
+    let x = document.getElementsByClassName(className);
+
+    // invert colors for each element
     for (let i = 0; i < x.length; i++) {
 
+        // get the computed style for the element
         var styles = window.getComputedStyle(x[i]);
 
-        let fgColor = styles.color;
-        let bgColor = styles.backgroundColor;
-        let bcColor = styles.borderColor;
-
-
-        fgColor = invertColor(fgColor);
-        bgColor = invertColor(bgColor);
-        bcColor = invertColor(bcColor);
-
-        x[i].style.color = fgColor;
-        x[i].style.backgroundColor = bgColor;
-        x[i].style.borderColor = bcColor;
+        // apply the inverted colors
+        x[i].style.color = invertColor(styles.color);
+        x[i].style.backgroundColor = invertColor(styles.backgroundColor);
+        x[i].style.borderColor = invertColor(styles.borderColor);
     }
 }
+
 
 function invertColor(rgb) {
 
+    // are the color channels sparated by comma or space?
     var sep = rgb.indexOf(",") > -1 ? "," : " ";
-    console.log("'" + sep + "'");
 
+    // remove everything but the color values from the string [rgb(]r, g, b[)]
     rgb = rgb.substr(rgb.indexOf("(") + 1).split(")")[0].split(sep);
 
-    for (let i = 0; i < rgb.length; i++) {
-        console.log("rgb[" + i + "] =", rgb[i]);
-    }
-
-    let rgbI = "rgb(" +
+    let rgbInverted = "rgb(" +
         (255 - parseInt(rgb[0])) + ", " +
         (255 - parseInt(rgb[1])) + ", " +
         (255 - parseInt(rgb[2])) + ")";
 
-    console.log(rgbI);
-    return rgbI;
+    // debugLog("rgbInverted = " + rgbInverted);
+    return rgbInverted;
+}
+
+function debugLog(...params) {
+
+    // return;
+    console.log(params);
 }
